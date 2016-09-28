164f8d0e12465796f9a1f38b289bd08580b28dbd
Guaidaodl <linyubinas@gmail.com>
2016-09-28 23:23:52 +0800

    ADD: LeakCanary library.

--- app/src/main/java/guaidaodl/github/io/pomodorotimer/PomodoroApplication.java
+++ app/src/main/java/guaidaodl/github/io/pomodorotimer/PomodoroApplication.java
@@ -2,13 +2,19 @@ package guaidaodl.github.io.pomodorotimer;
 
 import android.app.Application;
 
+import com.squareup.leakcanary.LeakCanary;
+
 import guaidaodl.github.io.pomodorotimer.service.PomodoroService;
 
 public class PomodoroApplication extends Application {
     @Override
     public void onCreate() {
         super.onCreate();
+        if (LeakCanary.isInAnalyzerProcess(this)) {
+            return;
+        }
 
+        LeakCanary.install(this);
         PomodoroService.start(this);
     }
 }
